18bb9ffe4f343788e76343da095fef5934c5194a
devallisamuel <alliolaoluwa@gmail.com>
2021-01-22 13:06:47 +0100

    final changes

---
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import  { ProductService  } from '../product.service';
+
+import  { Album } from  '../album'
+import { s } from '@angular/core/src/render3';
+@Component({
+  selector: 'app-product-tracklisting',
+  templateUrl: './product-tracklisting.component.html',
+  styleUrls: ['./product-tracklisting.component.css']
+})
+export class ProductTracklistingComponent implements OnInit {
+  albumInfo: Album;
+  constructor(private _productService:ProductService) { }
+
+  ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
+  }
+
+}
